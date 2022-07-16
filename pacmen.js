2d1fd2207234f9750f800f5b7a1e63a9b47a5c65
TingboW <104877939+TingboW@users.noreply.github.com>
2022-07-16 10:46:21 -0700

    lab assignment

--- pacmen.js
+++ pacmen.js
@@ -22,10 +22,11 @@ function makePac() {
   newimg.width = 100;
 
   // TODO: set position here
-
+  newimg.style.left = position.x;
+  newimg.style.top = position.y;
 
   // TODO add new Child image to game
-  game.appendChild(/* TODO: add parameter */);
+  game.appendChild(newimg); /* TODO: add parameter */
 
   // return details in an object
   return {
@@ -50,7 +51,12 @@ function update() {
 
 function checkCollisions(item) {
   // TODO: detect collision with all walls and make pacman bounce
-  
+  if (item.position.x + item.velocity.x + item.newimg.width >= window.innerWidth || item.position.x + item.velocity.x < 0){
+    item.velocity.x = -item.velocity.x;
+  }
+  if (item.position.y + item.velocity.y + item.newimg.height >= window.innerHeight || item.position.y + item.velocity.y < 0){
+    item.velocity.y = -item.velocity.y;
+  }
 }
 
 function makeOne() {
